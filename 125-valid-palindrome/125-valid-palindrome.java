76f114a330fa156f3f5d0e93fe80e28f0907ce09
Zepppelin <38655157+Zepppelin@users.noreply.github.com>
2022-08-17 12:32:05 +0530

    Time: 3 ms (98.90%), Space: 42.6 MB (92.27%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -2,32 +2,34 @@ class Solution
 {
     public boolean isPalindrome(String s) 
     {
-        char[] arr=s.toLowerCase().toCharArray();
-        int i =0;
-        int j = arr.length-1;
+        
+        s=s.toLowerCase();
+        int i=0;
+        int j= s.length()-1;
         
         while(i<j)
         {
-          if(!(arr[i]>='a' && arr[i]<='z' || arr[i]>='0' && arr[i]<='9'))
-          {
-              i++;
-          }
-          else if(!(arr[j]>='a' && arr[j]<='z' || arr[j]>='0' && arr[j]<='9'))
-          {
-              j--;
-          }
-          else
-          {
-              if(arr[i]==arr[j])
-              {
-                 i++;
-                 j--;
-              }
-              else
-              {
-                 return false; 
-              }
-          }
+           if(!(s.charAt(i)>='a' && s.charAt(i)<='z' || s.charAt(i)>='0' && s.charAt(i)<='9'))
+           {
+               i++;
+           }
+           else if(!(s.charAt(j)>='a' && s.charAt(j)<='z' || s.charAt(j)>='0' && s.charAt(j)<='9'))
+           {
+               j--;
+           }
+            else
+            {
+                 if(s.charAt(i)==s.charAt(j))
+                 {
+                     i++;
+                     j--;
+                 }
+                else
+                {
+                    return false;
+                }
+                
+            }
         }
         return true;
     }
