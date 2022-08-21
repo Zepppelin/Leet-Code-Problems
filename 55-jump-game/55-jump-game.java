e99f2517c27904ba3d345aeb53f5721e33268f50
Zepppelin <38655157+Zepppelin@users.noreply.github.com>
2022-08-21 23:01:09 +0530

    Time: 2 ms (93.29%), Space: 67.3 MB (74.20%) - LeetHub

---
+++ 55-jump-game/55-jump-game.java
@@ -0,0 +1,24 @@
+class Solution 
+{
+    public boolean canJump(int[] nums)
+    {
+        int goal = nums.length-1;
+        for(int i=nums.length-2;i>=0;i--)
+        {
+            
+            if(nums[i]+i>=goal)
+            {
+                goal = i;
+            }
+        }
+            
+        if(goal==0)
+        {
+           return true; 
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
